df942b35e7bb8705464c899df7c8f826d5f343ac
Andrew D Nguyen <nguye695@mail.chapman.edu>
2016-11-10 07:48:57 +0000

    added comments in print_stack

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -29,13 +29,21 @@ SVALUE pop_stack(STACK *s) {
     return val;
 }
 
+// prints the value passed in, of type SVALUE
 void print_svalue(SVALUE a) {
     printf ("%d", a);
 }
 
+// takes in a stack b
 void print_stack(STACK b) {
+    // calls the print_svalue and passes stack b's data value to be printed
+    // also prints a new line afterwards.
     print_svalue(b->data);
     printf("\n");
+    
+    // if stack b's head points to nothing, the function will end. 
+    // Otherwise, recursively calls the print_stack function
+    // passing in stack b's head, which points to the next element of the "stack"
     if (b->head == NULL){
         return;
     }
